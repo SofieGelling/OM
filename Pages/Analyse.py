46899627e73245b17cb33dd6adbd4bbe046524be
Sofie Gellings <sofie@gellings.com>
2025-05-01 11:39:18 +0200

    veranderingen

---
+++ Pages/Analyse.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("📊 Analyse")
+st.write("Hier kun je je analyse toevoegen.")
--- StreamLit.py
+++ StreamLit.py
@@ -1,24 +1,12 @@
 import streamlit as st
 import pandas as pd
-from OM import planning  # Zorg dat deze functie bestaat
+from OM import planning  
+from st_pages import add_page_title
+# pip install st-pages
+# streamlit run StreamLit.py
 
-st.set_page_config(page_title="Batch planner", layout="wide")
-st.title("📦 Open Batches Viewer")
+# -------------------------    Pagina met toggle    ---------------------------------------------------
 
-uploaded = st.file_uploader("Upload Excel-bestand", type="xlsx")
-
-if uploaded:
-    df = pd.read_excel(uploaded, sheet_name="Samples Release 2025")
-
-    # Toggle of expander om de grafiek zichtbaar te maken
-    show_plot = st.toggle("📊 Toon/verberg planning")
-
-    if show_plot:
-        fig = planning(df)
-        st.plotly_chart(fig, use_container_width=True)
-
-else:
-    st.info("👈 Voeg het 'TAT KPI' Excel-bestand toe om de tijdlijn te zien")
-
-## put this is command file
-## python -m streamlit run StreamLit.py
+st.set_page_config(page_title="Dashboard", layout="wide")
+st.title("Welkom bij je Batch Dashboard")
+st.markdown("👈 Kies een pagina in het zijmenu om te beginnen.")
