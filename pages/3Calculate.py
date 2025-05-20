17ff65fdd48de8439e8e742f31ae20d12b9ad929
D.Derks <512201@student.fontys.nl>
2025-05-20 13:28:41 +0200

    RODE target lijn toegevoegd

--- pages/3Calculate.py
+++ pages/3Calculate.py
@@ -15,9 +15,10 @@ if 'excel_df' in st.session_state:
              'Approval analyses',
              'Finish date QC',
              'Duedate',
-             'Type of samples']].copy()
+             'Type of samples',
+             'TAT Target']].copy()
 
-    df.columns = ['ProductID','Order', 'Received', 'Planned', 'Analyses', 'Approved', 'Finished', 'DueDate', 'Type']
+    df.columns = ['ProductID','Order', 'Received', 'Planned', 'Analyses', 'Approved', 'Finished', 'DueDate', 'Type', 'Target']
     df['Order'] = df['Order'].astype(str)
     df['Type'] = df['Type'].astype(str)
     df_n = df[df['Finished'].notna()].copy()
@@ -37,9 +38,27 @@ if 'excel_df' in st.session_state:
             if i + j < len(list_all_types):
                 t = list_all_types[i + j]
                 df_type = df_n[df_n['Type'] == t]
-
-                fig = px.box(df_type, y='Time_in_lab', title=f'Time in Lab - Type: {t}')
-                cols[j].plotly_chart(fig, use_container_width=True)
+                target_value = df_type['Target'].iloc[0] 
+                if len(df_type) < 5:
+                    with cols[j]:
+                        st.warning(f"⚠️ Too few samples for type '{t}' ({len(df_type)} entries)")
+                else:
+                    fig = px.box(df_type, y='Time_in_lab', title=f'Time in Lab - Type: {t}')
+                    fig.add_shape(
+                        type="line",
+                        x0=-0.5, x1=0.5,  # width of the box
+                        y0=target_value, y1=target_value,
+                        line=dict(color="red", width=2)
+                        )
+                    fig.add_annotation(
+                        y=target_value,
+                        x=0,  # box is centered on x=0
+                        text=f"Target: {target_value}",
+                        showarrow=False,
+                        yshift=10,
+                        font=dict(color="red")
+                        )
+                    cols[j].plotly_chart(fig, use_container_width=True)
 else:
     st.warning("⚠️ Upload an Excel file on the main page first.")
     
