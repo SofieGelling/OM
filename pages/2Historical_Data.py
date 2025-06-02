8fee2276d60b5537b1464c140c61ccc1fb4c86dc
Sofie Gellings <sofie@gellings.com>
2025-06-02 20:54:13 +0200

    Laatste aanpassing

--- pages/2Historical_Data.py
+++ pages/2Historical_Data.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from Functies import Boxplot
 
-st.set_page_config(page_title="Calculate", layout="wide")
-st.title("Calculations")
+st.set_page_config(page_title="Historical_Data", layout="wide")
+st.title("Boxplots")
 
 if 'excel_df' in st.session_state:
     Boxplot(st.session_state['excel_df'])
